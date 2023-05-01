b1a5198eff1faaf13ab59695fd80c2f424a2deec
SalehinShohan <ps.shohan1@gmail.com>
2023-05-01 22:37:40 +0600

    added all data in the servers UI

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () =>{
-    console.log('food data');
+const chefInfo = require('./data/chefInfo .json');
+const recipeInfo = require('./data/recipeInfo.json');
+
+app.get('/', (req, res) => {
+    res.send('Dragon is running')
+});
+
+app.get('/chefInfo', (req, res) => {
+    res.send(chefInfo);
+})
+
+app.get('/recipeInfo', (req, res) => {
+    res.send(recipeInfo);
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
 })
