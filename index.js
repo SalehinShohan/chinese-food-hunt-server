49e9aedbe05e1ca26f1ffe28da62f6576a55d68c
SalehinShohan <ps.shohan1@gmail.com>
2023-05-02 01:56:29 +0600

    check validatiin with each id of chefInfo

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ app.get('/chefInfo', (req, res) => {
     res.send(chefInfo);
 })
 
+app.get('/chef/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef = chefInfo.find(n=> n.id === id);
+    res.send(selectedChef);
+})
+
 app.get('/recipeInfo', (req, res) => {
     res.send(recipeInfo);
 })
 
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedRecipe= recipeInfo.filter(n => n._id === id);
+    res.send(selectedRecipe);
+})
+
 app.listen(port, () => {
-    console.log(`Dragon API is running on port: ${port}`)
+    console.log(`Chinese Food API is running on port: ${port}`)
 })
