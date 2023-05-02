ef0e993665b4d84e12183eb7e85f04e7d0e84644
SalehinShohan <ps.shohan1@gmail.com>
2023-05-02 17:20:01 +0600

    added more data info in chefIngo json file

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-const chefInfo = require('./data/chefInfo .json');
-const recipeInfo = require('./data/recipeInfo.json');
+const chefInfo = require("./data/chefInfo .json");
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Dragon is running')
+app.get("/", (req, res) => {
+  res.send("Dragon is running");
 });
 
-app.get('/chefInfo', (req, res) => {
-    res.send(chefInfo);
-})
+app.get("/chefInfo", (req, res) => {
+  res.send(chefInfo);
+});
 
-app.get('/chef/:id', (req, res) =>{
-    const id = req.params.id;
-    console.log(id);
-    const selectedChef = chefInfo.find(n=> n.id === id);
+app.get("/chefInfo/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+
+ 
+    const selectedChef = chefInfo.find((n) => n.id === id);
     res.send(selectedChef);
-})
-
-app.get('/recipeInfo', (req, res) => {
-    res.send(recipeInfo);
-})
-
-app.get('/recipe/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const selectedRecipe= recipeInfo.filter(n => n._id === id);
-    res.send(selectedRecipe);
-})
+  
+});
 
 app.listen(port, () => {
-    console.log(`Chinese Food API is running on port: ${port}`)
-})
+  console.log(`Chinese Food API is running on port: ${port}`);
+});
